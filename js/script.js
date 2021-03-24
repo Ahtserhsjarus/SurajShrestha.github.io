0d8c6625f00d0a44a8a8177353d25d5d643ee1fe
Suraj Shrestha <mejarus@gmail.com>
2021-03-24 16:26:00 +0545

    Demo Link update

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ const tableRow = document.querySelector(".table");
 
 fetch("https://api.github.com/users/Ahtserhsjarus/repos").then((res) =>
   res.json().then((data) => {
-    console.log(data[0]);
     let tr = `<tr>
     <th>SN</th>
     <th>Repositories
     </th>
     <th>Description</th>
     <th>URL Link</th>
+    <th>Demo Link</th>
     <th>Language</th>
 </tr>`;
 
@@ -18,7 +18,7 @@ fetch("https://api.github.com/users/Ahtserhsjarus/repos").then((res) =>
         <td>${repo.name}</td>
         <td>${repo.description}</td>
         <td><a href='${repo.html_url}' target=_blank><button>GO TO Repo</button></a></td>
-
+        <td><a href='https://${repo.owner.login}.github.io/${repo.name}/' target=_blank><button>Demo</button></a></td>
         <td>${repo.language}</td></tr>`;
       tableRow.innerHTML = tr;
     });
